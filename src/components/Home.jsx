06dc29401f1852a1243e779817ab50234cdce9a6
Sola <solakabuta@gmail.com>
2024-01-18 16:36:13 +0100

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     return ( 
         // MOBILE DISPLAY
         <>
-        <div className="w-screen h-screen home-container bg-blk 2xl:hidden">
+        <div className="w-screen h-screen home-container bg-blk min-[1440px]:hidden">
             <div className="grid texts place-items-center pt-60">
                 <h1 className="pb-20 font-bold">Welcome</h1> 
                 <h2 className="pb-20 text-2xl">Global Employer Services <br/> Reward & Mobility Alert</h2>
@@ -34,7 +34,7 @@ const Home = () => {
         <div className="h-screen bg-cover w-[50vw] max-[1440px]:hidden" style={{ backgroundImage: `url(${LoginBg})` }} >
         
         {/* CONTENT */}
-            <div className="grid pt-[15vw] place-items-center">
+            <div className="grid pt-[10vw] place-items-center">
                 <h1 className="pb-20 font-bold">Welcome</h1> 
                 <h2 className="pb-20 text-2xl">Global Employer Services <br/> Reward & Mobility Alert</h2>
                 <button id="create" className="h-20 mb-10 text-sm font-normal rounded-full w-52 bg-gradient-to-r from-btn to-rng">Create an account </button> 

91f658e393e887a0be810e8eb7bf030002874472
Sola <solakabuta@gmail.com>
2024-01-22 00:43:14 +0100

    webstormwtf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { useState } from 'react'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -11,17 +11,20 @@ import Settings from './components/Settings';
 function App() {
 
 
-  return (
-          <>
-          <div className='App'>
-            <Navbar />
-          </div>
-          <div className='content'>
-            <Home />
-            <Settings />
-          </div>
-          </>
-  )
+    return (
+        <Router>
+            <div className='App'>
+                <Navbar/>
+            </div>
+            <div className='content'>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/Settings' element={<Settings/>}/>
+                </Routes>
+
+            </div>
+        </Router>
+    )
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,14 @@
 import Logo from '../assets/Logos/Accenture.png'
 
 
+
 const Navbar = () => {
     return ( 
         <>
             <nav className="relative navbar min-[1440px]:hidden">
                 {/* Burger Menu */}
             
-                <a href="./Settings.jsx"><svg className='absolute scale-150 right-20 top-24' xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path d="M4 18l16 0" /></svg></a>
+                <a href="./Settings.jsx"><svg className='absolute scale-150 right-20 top-24' xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path d="M4 18l16 0" /></svg></a>
                 {/* Logo Accenture */}
                 <img id='logo' className='absolute w-20 left-16 top-16' src={Logo} alt="Accenture's logo"></img>
             </nav>

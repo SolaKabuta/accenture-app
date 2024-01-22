fa1c690910ce33c024bdaefb06cfeaa9b12da893
Sola <solakabuta@gmail.com>
2024-01-22 12:28:25 +0100

    framer-motion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 // import BurgerMenu from '../assets/Icons/Tabler.svg'
 import Logo from '../assets/Logos/Accenture.png'
-
+import {Link} from "react-router-dom";
 
 
 const Navbar = () => {
@@ -11,7 +11,9 @@ const Navbar = () => {
 
                 <a href="./Settings.jsx"><svg className='absolute scale-150 right-20 top-24' xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path d="M4 18l16 0" /></svg></a>
                 {/* Logo Accenture */}
-                <a href='/'><img id='logo' className='absolute w-20 left-16 top-16' src={Logo} alt="Accenture's logo"></img></a>
+                <div className='logo'>
+                    <a href='/'><img id='logo' className='absolute w-20 left-16 top-16' src={Logo} alt="Accenture's logo"></img></a>
+                </div>
             </nav>
         </>
     );
